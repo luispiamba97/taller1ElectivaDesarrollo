066d35fcf68ccdcd06de0f1f9cda6dd7c96b6f41
luispiamba97 <fernandopiamba97@gmail.com>
2026-09-03 13:35:21 -0500

    Se configura js para mostrar el mensaje en la consola de la pagina

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const boton = document.getElementById('btnHola');
+
+    boton.addEventListener('click', () => {
+        console.log("Hola Mundo desde JavaScript");
+    });
+});
